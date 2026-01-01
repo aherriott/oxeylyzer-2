8f10ae93eac6db75e3f00ebc62f58682bb0f47b3
Alex Herriott <ah.thewall@gmail.com>
2026-01-01 19:22:03 -0300

    AI fixed the types syntax

--- core/src/types.rs
+++ core/src/types.rs
@@ -1,42 +1,45 @@
 // Helper types
 
 // A Key value, like "A"
-typedef CacheKey usize;
+type CacheKey = usize;
 // A position for a Key
-typedef CachePos usize;
+type CachePos = usize;
 
 // An IndexVec is a Vec which itself stores indexes, and thus supports reverse lookup
-struct IndexVec {
+pub struct IndexVec {
     vec: Vec<usize>,
     reverse_vec: Vec<Option<usize>>,
 }
 
 impl IndexVec {
-    override [i: usize] {
-        // Return value
+    pub fn new() -> Self {
+        Self {
+            vec: Vec::new(),
+            reverse_vec: Vec::new(),
+        }
+    }
+
+    pub fn get(&self, i: usize) -> usize {
         self.vec[i]
     }
 
-    override [i: usize]
-        // When setting
-        self.vec[i] = set_value;
-        self.reverse_vec[set_value] = Some(i);
+    pub fn set(&mut self, i: usize, value: usize) {
+        self.vec[i] = value;
+        self.reverse_vec[value] = Some(i);
     }
 
-    pub fn iter(&self) -> Iter<usize> {
+    pub fn iter(&self) -> std::slice::Iter<usize> {
         self.vec.iter()
     }
 
-    pub fn reverse_at(&self, idx: usize) {
+    pub fn reverse_at(&self, idx: usize) -> Option<usize> {
         self.reverse_vec[idx]
     }
 
-    pub fn with_capacity(&mut self, capacity: usize) -> Self {
+    pub fn with_capacity(capacity: usize) -> Self {
         Self {
             vec: Vec::with_capacity(capacity),
-            reverse_vec: vec![0; capacity],
+            reverse_vec: vec![None; capacity],
         }
     }
 }
-
-
